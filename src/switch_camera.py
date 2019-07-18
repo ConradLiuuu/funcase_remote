5c6592e58a986e6b0eef09908dad79f58a1577be
ConradLiuuu <1104104320@gm.kuas.edu.tw>
2019-07-18 10:36:44 +0800

    switch camera node turn into rpi control

--- src/switch_camera.py
+++ src/switch_camera.py
@@ -1,39 +1,31 @@
 #!/usr/bin/env python
-import sys
-from time import sleep
-sys.path.append('/opt/nvidia/jetson-gpio/lib/python/')
-sys.path.append('/opt/nvidia/jetson-gpio/lib/python/GPIO')
-import Jetson.GPIO as gpio
 import rospy
-#from funcase_client.msg import JoyCmd
 from std_msgs.msg import Bool
+import RPi.GPIO as gpio
 
 gpio.setmode(gpio.BCM)
-switch = 18
-gpio.setup(switch, gpio.OUT, initial = gpio.LOW)
-#gpio.setwarnings(False)
-swit = False
+switch_pin = 17
+
+gpio.setup(switch_pin,gpio.OUT)
+gpio.output(switch_pin,0)
+switch = False
 
 def callback(data):
-    global swit
-    #joy_cmd = data
-    #button = joy_cmd.joy[0].encode("hex")
-    sum_ = data.data
-    #print(c)
-    
-    if sum_ == True:
-        swit = True
-        gpio.output(switch,gpio.HIGH)
-    if sum_ == False:
-        swit = False
-        gpio.output(switch,gpio.LOW)
-    print(swit)
-     
+    global switch
+    swit = data.data
+
+    if swit == True:
+        switch = True
+        gpio.output(switch_pin, 1)
+    if swit == False:
+        switch = False
+        gpio.output(switch_pin, 0)
+    print "switch is ",switch
+
 def listener():
-    rospy.init_node('switch_camera')
+    rospy.init_node("switch_camera")
     rospy.Subscriber("/switch_camera", Bool, callback, queue_size=1)
 
 while not rospy.is_shutdown():
     listener()
     rospy.spin()
-
