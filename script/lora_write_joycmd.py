5b1c497acf0690c96c5651b82a6e50f4f082685c
ConradLiuuu <1104104320@gm.kuas.edu.tw>
2019-07-29 10:40:49 +0800

    debug lora_write_joycmd

--- script/lora_write_joycmd.py
+++ script/lora_write_joycmd.py
@@ -28,8 +28,8 @@ class lora_write_joycmd:
     def callback(self, data):
         self.axe = data.axes
         self.button = data.buttons
-        seg_axes = []
-        enc_axes = []
+        seg_axes = [0,0,0,0,0,0,0,0]
+        enc_axes = [0,0,0,0,0,0,0,0]
 
         ## processing axes
         for i in range(0,8):
@@ -66,7 +66,7 @@ class lora_write_joycmd:
         self.sum_axe_below = enc_axes[2]*64 + enc_axes[5]*16 + enc_axes[6]*4 + enc_axes[7]*1
         self.sum_button = self.button[0]*64 + self.button[1]*32 + self.button[2]*16 + self.button[3]*8 + self.button[7]*4 + self.button[6]*2 + self.button[8]*1
 
-        self.joy_cmd = header + str(self.sum_button) + sep + str(self.sum_axe_front) + sep + str(self.sum_axe_below) +sep
+        self.joy_cmd = self.header + str(self.sum_button) + self.sep + str(self.sum_axe_front) + self.sep + str(self.sum_axe_below) +self.sep
         self.LoRa.FunLora_5_write16bytesArrayString(self.joy_cmd)
         print "joy_cmd = ", self.joy_cmd
 
